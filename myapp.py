72c1fe99f6672983db1d6ffdd42e23b32c9b874d
Arnab07 <aroy34899@gmail.com>
2025-11-25 11:08:13 +0530

    First commit

---
+++ myapp.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return {
+        "status": "SUCCESS",
+        "data":{
+            "regno": 1847207,
+            "name": "Arnab",
+            "email": "aroy1243@gmail.com"
+        }
+    }
+
+if __name__ == "__main__":
+    app.run(debug=true)
